29641394f82fcde4deadf00cbc7cf9803646164a
Annestezia <cast.wheel@gmail.com>
2019-07-29 15:07:58 +1000

    working version from cp

---
+++ script.js
@@ -0,0 +1,51 @@
+  const newQuote = () => {
+    const n = 30;
+    const rand = num => {
+      return Math.floor(Math.random() * num);
+    };
+    const random = rand(n);
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${n}`
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        var content = json[random].content.replace(/<[^>]*>?/gm, ""),
+          title = json[random].title,
+          linkBase = "https://twitter.com/intent/tweet?text=";
+        var tweetLink = `${linkBase}${content} -- ${title}`,
+          tweetEl = document.getElementsByClassName("twitter-share-button")[0];
+        tweetEl.setAttribute("href", tweetLink);
+        tweetEl.setAttribute("target", "_blank");
+        document.getElementById("quote").innerHTML = content;
+        document.getElementById("author").innerHTML = title;
+      });
+    var newQuoteButton = document.getElementById("new");
+  };
+
+  newQuote();  
+
+
+
+
+
+//oldschool  version =))
+
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//     document.getElementById("quote").innerHTML = JSON.parse(
+//       this.responseText
+//     )[random].content;
+//     document.getElementById("author").innerHTML = JSON.parse(
+//       this.responseText
+//     )[random].title;
+//   }
+// };
+// xhttp.open(
+//   "GET",
+//   `https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${n}`,
+//   true
+// );
+// xhttp.send();
