3cf54a89964761b9df7d42c9687567eeb4dda8fd
Annestezia <cast.wheel@gmail.com>
2021-02-09 23:33:07 +1000

    removed cors-anywhere

--- script.js
+++ script.js
@@ -8,21 +8,37 @@ const newQuote = () => {
             return Math.floor(Math.random() * num);
           };
     const random = rand(n); 
-    
-    fetch(`https://cors-anywhere.herokuapp.com/https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e4b495e4f62a650fb0db0405c20fa4f48cb75a0e/quotes.json`)
-      .then(function(response) { return response.json();})
-      .then(function(json) {
-      let {quote,author}=json.quotes[random];      
+
+
+    // You can access that like this:
+
+    // fetch('https://api.github.com/gists/53e1780a5a68fe9281cfbbc9820d381f')
+    //   .then(results => {
+    //     return results.json();
+    //   })
+    //   .then(data => {
+    //     console.log(data.files["forgeLikeServerSetup.md"].content);
+    //   });
+
+
+    fetch(`https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e4b495e4f62a650fb0db0405c20fa4f48cb75a0e/quotes.json`)
+      .then(res=> res.json())
+      .then((json) =>{
+      const {quote,author}=json.quotes[random];
       const linkBase = "https://twitter.com/intent/tweet?text=",      
            tweetEl = document.getElementsByClassName("twitter-share-button")[0];
-           let escaped=encodeURI(quote);
-      let tweetLink = `${linkBase}${escaped} -- ${author}`;
+           const escaped=encodeURI(quote);
+      const tweetLink = `${linkBase}${escaped} -- ${author}`;
         tweetEl.setAttribute("href", tweetLink);
         tweetEl.setAttribute("target", "_blank");
         document.getElementById("quote").innerHTML = quote;
         document.getElementById("author").innerHTML = author;
+
+      }).catch((err) => {
+        console.log(err);
+        // console.error('Error:', err);
       });
-    const newQuoteButton = document.getElementById("new");
+    // const newQuoteButton = document.getElementById("new");
   };
 
   newQuote();   
