814c32eb915df5f429b1edcacd6159fa1e153dae
Annestezia <cast.wheel@gmail.com>
2021-03-19 01:47:14 +1000

    clean-up, --comments

--- script.js
+++ script.js
@@ -9,18 +9,6 @@ const newQuote = () => {
           };
     const random = rand(n); 
 
-
-    // You can access that like this:
-
-    // fetch('https://api.github.com/gists/53e1780a5a68fe9281cfbbc9820d381f')
-    //   .then(results => {
-    //     return results.json();
-    //   })
-    //   .then(data => {
-    //     console.log(data.files["forgeLikeServerSetup.md"].content);
-    //   });
-
-
     fetch(`https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e4b495e4f62a650fb0db0405c20fa4f48cb75a0e/quotes.json`)
       .then(res=> res.json())
       .then((json) =>{
@@ -36,29 +24,7 @@ const newQuote = () => {
 
       }).catch((err) => {
         console.log(err);
-        // console.error('Error:', err);
       });
-    // const newQuoteButton = document.getElementById("new");
   };
 
   newQuote();   
-
-//oldschool  version =)) 
-
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//   if (this.readyState == 4 && this.status == 200) {
-//     document.getElementById("quote").innerHTML = JSON.parse(
-//       this.responseText
-//     )[random].content;
-//     document.getElementById("author").innerHTML = JSON.parse(
-//       this.responseText
-//     )[random].title;
-//   }
-// };
-// xhttp.open(
-//   "GET",
-//   `https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${n}`,
-//   true
-// );
-// xhttp.send();
